643d4828c810ae2f272737e83212072b1b30a339
vivian.yu <vivian.yu@dianping.com>
2017-03-01 18:36:54 +0800

    email test

--- A4/bayes.py
+++ A4/bayes.py
@@ -73,6 +73,85 @@ def testingNB():
 
 
 
+def bagOfWords2VecMN(vocabList, inputSet):
+	returnVec = [0]*len(vocabList)
+	for word in inoutSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] += 1
+	return returnVec
+
+
+
+
+def textParse(bigString):
+	import re
+	listOfTokens = re.split(r'\W*', bigString)
+	return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+
+def spamTest():
+	docList = []; classList = []; fullText = []
+	for i in range(1, 26):
+		wordList = textParse(open('email/spam/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		wordList = textParse(open('email/ham/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	print 'classList : %s' % classList
+	vocabList = createVocabList(docList)
+	trainingSet = range(50); testSet = []
+	for i in range(10):
+		randIndex = int(random.uniform(0, len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat=[]; trainClasses = []
+	for docIndex in trainingSet:
+		trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+		trainClasses.append(classList[docIndex])
+
+	print 'trainClasses %s ' %trainClasses
+	print len(trainMat)
+	p0V, p1V, pSam = trainNB0(array(trainMat), array(trainClasses))
+	print p0V  ,len(p0V)
+	print p1V ,len(p1V)
+	print pSam 
+	errorCount = 0
+
+	for docIndex in testSet:
+		wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+		if classifyNB(array(wordVector), p0V, p1V, pSam) != classList[docIndex] :
+			errorCount += 1
+
+	print 'the error rate is : ', float(errorCount)/len(testSet)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
