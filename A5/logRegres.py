6955edb7970cdfcb17bf75fecb62543981ada02c
vivian.yu <vivian.yu@dianping.com>
2017-03-03 18:59:37 +0800

    ch05 colicTest

--- A5/logRegres.py
+++ A5/logRegres.py
@@ -78,6 +78,48 @@ def stocGradAscent1(dataMatrix,classLabels, numIter=150):
 			error = classLabels[randIndex] - h
 			weights = weights + alpha * error * dataMatrix[randIndex]
 			del(dataIndex[randIndex])
-			print randIndex
-			print dataIndex
+			# print randIndex
+			# print dataIndex
 	return weights
+
+def classifyVector(inX, weights):
+	prob = sigmod(sum(inX*weights))
+	if prob > 0.5:
+		return 1.0
+	else:
+		return 0.0
+
+def colicTest():
+	frTrain = open('horseColicTraining.txt')
+	frTest = open('horseColicTest.txt')
+	trainingSet = []; trainingLables = []
+	for line in frTrain.readlines():
+		currLine = line.strip().split('\t')
+		lineArr = []
+		for i in range(21):
+			lineArr.append(float(currLine[i]))
+
+		trainingSet.append(lineArr)
+		trainingLables.append(float(currLine[21]))
+
+	trainWeights = stocGradAscent1(array(trainingSet), trainingLables, 500)
+	errorCount = 0; numTestVec = 0.0
+	for line in frTest.readlines():
+		numTestVec += 1.0
+		currLine = line.strip().split('\t')
+		lineArr = []
+		for i in range(21):
+			lineArr.append(float(currLine[i]))
+		if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+			errorCount += 1
+	errorRate = (float(errorCount)/numTestVec)
+	print "the error rate of test is: %f" % errorRate
+	return errorRate
+
+def multiTest():
+	numTests = 10; errorSum = 0.0
+	for k in range(numTests):
+		errorSum += colicTest()
+	print "after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests))
+
+
